31dbedda7cd89f02704abb34d752c1a98b19e973
Natán Verdés <natverber@gmail.com>
2017-04-11 17:54:32 +0200

    Example of input component property usage and component inside component usage, in user-list component.

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
-  name: string; // <-- added name property
+  @Input() name: string; // <-- added Input annotation
 
   constructor() {
-    this.name = 'Felipe'; // set the name
   }
 
   ngOnInit() {
