78d3f8e3154a87a0ce463b2b1e3f236ce186845c
Natán Verdés <natverber@gmail.com>
2017-04-11 17:36:01 +0200

    Generated user-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
