92d226f309c0175e2c5837ef221389d77080742a
Natán Verdés <natverber@gmail.com>
2017-04-11 18:42:29 +0200

    Deleted old example components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { UserItemComponent } from './user-item/user-item.component';
-import { UserListComponent } from './user-list/user-list.component';
 import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent,
-    UserItemComponent,
-    UserListComponent,
     ArticleComponent
   ],
   imports: [
