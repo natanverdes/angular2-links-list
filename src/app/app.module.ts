c0078881b3bc70118cd240fe6ecba00c992c488e
Natán Verdés <natverber@gmail.com>
2017-04-11 18:07:31 +0200

    Generated component article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     UserItemComponent,
-    UserListComponent
+    UserListComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
