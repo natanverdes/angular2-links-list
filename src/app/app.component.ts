678bb1e84fc830662bee8ab46b5561c830f55240
Natán Verdés <natverber@gmail.com>
2017-04-11 18:04:28 +0200

    Example of click button usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  addArticle(): boolean {
+    console.log("Adding article");
+    return false;
+  }
 }
