dfd787fb045ecee45f5523e58315aab8e0ffb4b4
Natán Verdés <natverber@gmail.com>
2017-04-11 18:30:12 +0200

    Allowed to add new articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,9 @@ export class AppComponent {
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 
