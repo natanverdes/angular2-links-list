5a0195ae07d63c79018cab1914b8b62dcfee99e4
Natán Verdés <natverber@gmail.com>
2017-04-11 18:26:45 +0200

    Articles now are rendered with ngFor inside app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Article} from "./article/article.model";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 
+  articles: Article[];
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
+  }
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
   }
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
 import {Article} from "./article.model";
 
 @Component({
@@ -8,13 +8,9 @@ import {Article} from "./article.model";
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input() article: Article;
 
   constructor() {
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10);
   }
 
   voteUp() {
